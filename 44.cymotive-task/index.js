99a99666e84fc7e665e4b8b6842955bdc319083d
11carmel11 <11carmrl11@gmail.com>
2022-01-25 13:46:04 +0200

    Phase 1

---
+++ 44.cymotive-task/index.js
@@ -0,0 +1,11 @@
+const axios = require("axios");
+const reports = require("./reports.json");
+
+const API = "https://y2l13tgqj0.execute-api.eu-west-2.amazonaws.com/api";
+
+const submit = async (report) => {
+  const { data } = await axios.post(API, { body: { report } });
+  console.log(data);
+};
+
+// reports.forEach(submit);
